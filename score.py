465816a4b8850a29757fbcee4f8b432720a0207b
Pathak <dixit.pathak@accenture.com>
2020-01-21 14:48:17 +0530

    First Commit'

---
+++ score.py
@@ -0,0 +1,23 @@
+from sklearn.externals import joblib
+from sklearn.linear_model import Ridge
+from azureml.core.model import Model
+import numpy
+import json
+
+def init():
+    global model
+    model_path = Model.get_model_path('sklearn_mnist_model.pkl')
+    # deserialize the model file back into a sklearn model
+    model = joblib.load(model_path)
+
+# note you can pass in multiple rows for scoring
+def run(raw_data):
+    try:
+        data = json.loads(raw_data)['data']
+        data = numpy.array(data)
+        result = model.predict(data)
+        # you can return any datatype if it is JSON-serializable
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
